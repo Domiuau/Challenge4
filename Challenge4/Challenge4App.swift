9189d59969a6825570630058de945f3d0a6ff2a5
domiuau <domiuaunoob@outlook.com>
2025-01-29 14:31:10 -0300

    Initial Commit

---
+++ Challenge4/Challenge4App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge4App.swift
+//  Challenge4
+//
+//  Created by GUILHERME MATEUS SOUSA SANTOS on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
