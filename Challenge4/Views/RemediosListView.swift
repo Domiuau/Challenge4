2aa4d04d832565fc00fd4567356237b4fc2d454b
amanda <amandacsr10@gmail.com>
2025-01-31 16:10:55 -0300

    dismiss added

--- Challenge4/Views/RemediosListView.swift
+++ Challenge4/Views/RemediosListView.swift
@@ -15,40 +15,40 @@ struct RemediosListView: View {
         ScrollView {
             NavigationView {
                 VStack{
-                    NavigationLink("Adicionar Remédio", destination: AdicionarRemedio(vm: vm))
-                    
                     List {
-                        HStack {
-                            ForEach(vm.entidadeSalvasRemedio) { entidade in
+                        ForEach(vm.entidadeSalvasRemedio) { entidade in
+                            HStack {
                                 if let data = entidade.imagem {
                                     Image(uiImage: UIImage(data: data)!.resized(to:200)!)
                                         .resizable()
                                         .aspectRatio(contentMode: .fit)
-                                    // .frame(width: 50, height: 50)
                                 } else {
                                     Image(uiImage: UIImage(named: "remedios")!.resized(to:200)!)
                                         .resizable()
                                         .aspectRatio(contentMode: .fit)
-                                    // .frame(width: 50, height: 50)
                                 }
                                 
                                 VStack (alignment: .leading) {
                                     Text("**\(entidade.nomeRemedio ?? "SEM NOME")**")
                                         .padding(.vertical)
-                                    Text("Dosagem: **\(entidade.horario ?? "SEM HORARIO")**")
-                                    Text("Horário: **\(entidade.dosagem ?? "SEM DOSAGEM")**")
+                                    Text("Dosagem: **\(entidade.dosagem ?? "SEM HORARIO")**")
+                                    Text("Horário: **\(entidade.horario ?? "SEM DOSAGEM")**")
                                     
                                 }
                                 .padding(10)
                             }
                         }
-                        .frame(height: 100)
                         .padding(10)
                     }
                 }
             }
         }
         .navigationTitle("Remédios")
+        .toolbar {
+            ToolbarItem {
+                NavigationLink("Adicionar Remédio", destination: AdicionarRemedio(vm: vm))
+            }
+        }
     }
 }
 
