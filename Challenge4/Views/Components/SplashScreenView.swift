70147305192492621d01fbf6d77fa8f08a218086
amanda <amandacsr10@gmail.com>
2025-02-04 15:18:12 -0300

    splashScreen meio feita, ainda vou melhorar

---
+++ Challenge4/Views/Components/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  HearTrack
+//
+//  Created by AMANDA CAROLINE DA SILVA RODRIGUES on 04/02/25.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
---
+++ Challenge4/Views/Components/SplashScreenView2.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView2.swift
+//  HearTrack
+//
+//  Created by AMANDA CAROLINE DA SILVA RODRIGUES on 04/02/25.
+//
+
+import SwiftUI
+
+struct SplashScreenView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView2()
+}
