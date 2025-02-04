ccf28db344dcb817b401c90250f482d7c6da9afc
amanda <amandacsr10@gmail.com>
2025-02-04 15:18:32 -0300

    splash screen meio feita so que agora com staged

--- Challenge4/Views/Components/SplashScreenView.swift
+++ Challenge4/Views/Components/SplashScreenView.swift
@@ -7,12 +7,34 @@
 
 import SwiftUI
 
-struct SplashScreenView: View {
+struct SplashScreenView2: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Path { path in
+                path.move(to: CGPoint(x:125, y:350))
+                path.addLine(to: CGPoint(x: 150, y: 300))
+                path.addLine(to: CGPoint(x: 185, y: 350))
+                path.addLine(to: CGPoint(x: 215, y: 300))
+                path.addLine(to: CGPoint(x: 245, y: 350))
+                path.addLine(to: CGPoint(x: 185, y: 450))
+                path.addLine(to: CGPoint(x: 125, y: 350))
+                
+            }.stroke(Color.black)
+            
+            Path { path in
+                path.move(to: CGPoint(x: 125, y: 350))
+                path.addEllipse(in: CGRect(x: 120, y: 341, width: 15, height: 15))
+                path.addEllipse(in: CGRect(x: 142.5, y: 299, width: 15, height: 15))
+                path.addEllipse(in: CGRect(x: 179, y: 341, width: 15, height: 15))
+                path.addEllipse(in: CGRect(x: 207, y: 299, width: 15, height: 15))
+                path.addEllipse(in: CGRect(x: 120, y: 341, width: 15, height: 15))
+                path.addEllipse(in: CGRect(x: 120, y: 341, width: 15, height: 15))
+                path.addEllipse(in: CGRect(x: 120, y: 341, width: 15, height: 15))
+            }
+        }
     }
 }
 
 #Preview {
-    SplashScreenView()
+    SplashScreenView2()
 }
--- Challenge4/Views/Components/SplashScreenView2.swift
+++ Challenge4/Views/Components/SplashScreenView2.swift
@@ -7,12 +7,74 @@
 
 import SwiftUI
 
-struct SplashScreenView2: View {
+struct SplashScreenView: View {
+    @ObservedObject var vm = PressaoViewModel()
+    
+    @State private var drawProgress1: CGFloat = 0.0
+    @State private var drawProgress2: CGFloat = 0.0
+    @State private var drawProgress3: CGFloat = 0.0
+    @State private var isActive = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isActive{
+            TabBarComponent(pressaoViewModel: vm)
+        }else{
+            VStack{
+                ZStack {
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                        .mask(
+                            Rectangle()
+                                .size(width: 200 * drawProgress1, height: 99)
+                                .offset(x: -100 + (200 * drawProgress1) / 2, y: 0)
+                        )
+                        .onAppear {
+                            withAnimation(.linear(duration: 1.0)) {
+                                drawProgress1 = 1.0
+                            }
+                        }
+                    
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                        .mask(
+                            Rectangle()
+                                .size(width: 200 * drawProgress2, height: 101)
+                                .offset(x: 200 * (1 - drawProgress2) , y:99)
+                        )
+                        .onAppear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                                withAnimation(.linear(duration: 1.0)) {
+                                    drawProgress2 = 1.0
+                                }
+                            }
+                        }
+                }.onAppear{
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 10.0){
+                        self.isActive = true
+                    }
+                }
+                
+                Image("appNome")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(40)
+                    .opacity(drawProgress3)
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                            withAnimation(.linear(duration: 1.0)) {
+                                drawProgress3 = 1.0
+                            }
+                        }
+                    }
+            }
+        }
     }
 }
 
+
 #Preview {
-    SplashScreenView2()
+    SplashScreenView()
 }
