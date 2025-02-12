74c4e1f3959d9647db63159515c104280466c78f
domiuau <domiuaunoob@outlook.com>
2025-02-12 14:15:57 -0300

    testes mas ta quebrado

---
+++ Challenge4/Models/PressaoModel.swift
@@ -0,0 +1,8 @@
+//
+//  PressaoModel.swift
+//  HearTrack
+//
+//  Created by GUILHERME MATEUS SOUSA SANTOS on 11/02/25.
+//
+
+import Foundation
---
+++ Challenge4/Models/RemediosModel.swift
@@ -0,0 +1,8 @@
+//
+//  RemediosModel.swift
+//  HearTrack
+//
+//  Created by GUILHERME MATEUS SOUSA SANTOS on 11/02/25.
+//
+
+import Foundation
